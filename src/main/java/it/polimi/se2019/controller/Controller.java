94e9c1299f80d6018ec40dfbc3269838825d45ca
Marco Melis <marco.melis@mail.polimi.it>
2019-04-07 21:55:51 +0200

    add java file view and controller

---
+++ src/main/java/it/polimi/se2019/controller/Controller.java
@@ -0,0 +1,48 @@
+public class Controller {
+  //references to the player who's performing actions and the match state
+  ArrayList <Player> player;
+  ArrayList <Match> match;
+  //each player draws two cards in their first turn and discard one
+  public void startingDraw(){
+
+  }
+  //it places the player figure back on the board by drawing a powerup card
+  public void resurrect(){
+
+  }
+  //player's movement action, up to three tiles (standard)
+  public void movement(){
+
+  }
+  //player's movement&grab action, up to 1 tile and grabbing a weapon or ammo.
+  public void moveGather(){
+
+  }
+  //selecting a weapon among the three available in the resurrection room (when buying) or before shooting.
+  public void chooseWeapon(){
+
+  }
+
+  //player's shooting ability
+  public void shoot(){
+
+  }
+  //a weapon is reloaded by paying its cost,
+  public void reloadWeapons(){
+
+  }
+  //it fills back weapons and ammo cards grabbed during the previous turn
+  public void restoreMap(){
+
+  }
+  //the view is updated each time a change occurs.
+  public void updateView(){
+
+  }
+  //at the end of each turn the game has to perform several tasks: refilling
+  //the weapons slot, ammo and resurrecting players killed. 
+  public void endTurn(){
+
+  }
+
+}
---
+++ src/main/java/it/polimi/se2019/view/View.java
@@ -0,0 +1,26 @@
+public class View {
+  //the number of point of the current player
+  public void showPoints(){
+
+  }
+	//the number of marks 
+  public void showMarks(){
+
+  }
+	//the number of cubes of all players
+  public void showCubes(){
+
+  }
+	//the position of each player on the battlefield
+  public void showPlayers(){
+
+	}
+	//the weapons owned or on the field
+	public void showWeapons(){
+	
+	}
+	//the current powerups owned 
+	public void showPowerUps(){
+	
+	}
+}
