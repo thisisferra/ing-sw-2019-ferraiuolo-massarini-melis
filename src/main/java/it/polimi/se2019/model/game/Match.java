d860f02562252d5051d372675d92bae2ff1a55b4
merklind <marco.melis@mail.polimi.it>
2019-04-15 10:31:52 +0200

    Add pickUp method and other supports methods

--- src/main/java/it/polimi/se2019/model/game/Match.java
+++ src/main/java/it/polimi/se2019/model/game/Match.java
@@ -1,11 +1,14 @@
 package it.polimi.se2019.model.game;
 
+import com.google.gson.Gson;
 import it.polimi.se2019.model.cards.Ammo;
 import it.polimi.se2019.model.cards.PowerUp;
 import it.polimi.se2019.model.cards.Weapon;
 import it.polimi.se2019.model.map.WeaponSlot;
 import it.polimi.se2019.model.player.Player;
 import java.util.ArrayList;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 public class Match {
     private ArrayList<Player> players;
@@ -25,10 +28,33 @@ public class Match {
     }
     //generate the gamefield
     public void initGameField(){
-
+        Gson gson = new Gson();
+        try {
+            ammoStack = gson.fromJson(new FileReader("/src/main/java/it/polimi/se2019/ammo.json"), ArrayList.class);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        for(Ammo ammo : ammoStack)
+            System.out.println(ammoStack.toString());
     }
     public void round(Player player){
 
+    }
+
+    public Ammo pickUpAmmoStack() {
+        int size = ammoStack.size();
+        return ammoStack.get(size-1);
+    }
+
+    public PowerUp pickUpPowerUp() {
+        int size = powerUpStack.size();
+        return powerUpStack.get(size - 1);
+    }
+
+    public void discardAmmo(Ammo currentAmmo) {
+        //currentAmmo is the last ammo I've picked up
+        int size = discardedAmmos.size();
+        discardedAmmos.add(size - 1, currentAmmo);
     }
 
 }
