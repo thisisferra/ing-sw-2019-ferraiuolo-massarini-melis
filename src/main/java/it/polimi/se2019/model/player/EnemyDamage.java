84fcd4a18546b1540a2ca97bf7a7d24626e5cc37
merklind <marco.melis@mail.polimi.it>
2019-04-27 19:02:02 +0200

    Add EnemyDamage class and added some method

---
+++ src/main/java/it/polimi/se2019/model/player/EnemyDamage.java
@@ -0,0 +1,29 @@
+package it.polimi.se2019.model.player;
+
+
+public class EnemyDamage{
+    private Player aggressorPlayer;
+    private int damage;
+    private boolean firstShot;
+
+    public int getDamage() {
+        return this.damage;
+    }
+
+    public void setDamage(int damage) {
+            this.damage = damage;
+        }
+
+    public boolean getFirstShot(){
+        return this.firstShot;
+    }
+
+    public Player getAggressorPlayer() {
+        return this.aggressorPlayer;
+    }
+
+    public void setAggressorPlayer(Player aggressorPlayer){
+        this.aggressorPlayer = aggressorPlayer;
+    }
+
+}
