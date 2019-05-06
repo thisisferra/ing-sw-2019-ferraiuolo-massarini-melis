8066c081893f45d318d5f229abc56c0a7ec29523
merklind <marco.melis@mail.polimi.it>
2019-05-06 18:15:59 +0200

    Add custom constructor EnemyDamage class

--- src/main/java/it/polimi/se2019/model/player/EnemyDamage.java
+++ src/main/java/it/polimi/se2019/model/player/EnemyDamage.java
@@ -4,10 +4,24 @@ package it.polimi.se2019.model.player;
 public class EnemyDamage{
     private Player aggressorPlayer;
     private int damage;
-    private boolean firstShot; //TODO da implementare
+
+    public EnemyDamage(Player aggressorPlayer, int damage) {
+        this.aggressorPlayer = aggressorPlayer;
+        this.damage = damage;
+    }
+
+    public EnemyDamage(){}
+
+    public Player getAggressorPlayer() {
+        return this.aggressorPlayer;
+    }
 
     public int getDamage() {
         return this.damage;
+    }
+
+    public void setAggressorPlayer(Player aggressorPlayer){
+        this.aggressorPlayer = aggressorPlayer;
     }
 
     public void setDamage(int damage) {
@@ -19,22 +33,6 @@ public class EnemyDamage{
                 this.damage = 11;
             }
         }
-    }
-
-    public void setFirstShot(boolean bool) {
-        this.firstShot = bool;
-    }
-
-    public boolean getFirstShot(){
-        return this.firstShot;
-    }
-
-    public Player getAggressorPlayer() {
-        return this.aggressorPlayer;
-    }
-
-    public void setAggressorPlayer(Player aggressorPlayer){
-        this.aggressorPlayer = aggressorPlayer;
     }
 
 }
